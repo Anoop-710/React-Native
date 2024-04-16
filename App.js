1bf56045751abc01e3b427678fca8b243dfb0b90
anoop <anoopaathreya@gmail.com>
2024-04-16 18:22:27 +0530

    View, Text, image, bgImage, basicStyling

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, Image, ImageBackground } from "react-native";
+import logoImage from "./assets/icon.png";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+      <Text>
+        <Text style={{ color: "white", fontWeight: "bold", fontSize: 30 }}>
+          Hello
+        </Text>
+        World
+      </Text>
+
+      {/* <Image
+        source={logoImage}
+        style={{ width: 200, height: 200, alignSelf: "center" }}
+      />
+      <Image
+        source={{ uri: "https://picsum.photos/500" }}
+        style={{ width: 200, height: 200, alignSelf: "center" }}
+      /> */}
+
+      <ImageBackground
+        source={{ uri: "https://picsum.photos/600" }}
+        style={{ flex: 1 }}
+      >
+        <Text
+          style={{
+            color: "white",
+            fontWeight: "bold",
+            fontSize: 30,
+          }}
+        >
+          Image text
+        </Text>
+      </ImageBackground>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
