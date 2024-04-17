07bf60766f92c8069e2ac7cfc19d55c208786827
anoop <anoopaathreya@gmail.com>
2024-04-17 12:34:32 +0530

    custom components

--- App.js
+++ App.js
@@ -13,29 +13,13 @@ import {
 } from "react-native";
 import logoImage from "./assets/icon.png";
 import { useState } from "react";
+import Greet from "./components/Greet";
 export default function App() {
   const [showModal, setShowModal] = useState(false);
   const [showStatusBar, setShowStatusBar] = useState(false);
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 20 }}>
-      <Button title="Alert" onPress={() => Alert.alert("Invalid Data")} />
-      <Button
-        title="Alert 2"
-        onPress={() => Alert.alert("Invalid Data", "DOB is not valid")}
-      />
-      <Button
-        title="Alert 3"
-        onPress={() =>
-          Alert.alert("Invalid Data", "DOB is not valid", [
-            {
-              text: "Cancel",
-              onPress: () => console.log("Cancel Pressed"),
-              style: "cancel",
-            },
-            { text: "OK", onPress: () => console.log("OK Pressed") },
-          ])
-        }
-      />
+      <Greet name="Bruce Wayne" />
     </View>
   );
 }
