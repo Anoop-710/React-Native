b38da279d615a2c9f425c92e5483d26a477f8774
anoop <anoopaathreya@gmail.com>
2024-04-17 13:06:52 +0530

    stylesheet api

--- App.js
+++ App.js
@@ -1,25 +1,19 @@
-import {
-  View,
-  Text,
-  Image,
-  ImageBackground,
-  ScrollView,
-  Button,
-  Pressable,
-  Modal,
-  StatusBar,
-  ActivityIndicator,
-  Alert,
-} from "react-native";
-import logoImage from "./assets/icon.png";
-import { useState } from "react";
-import Greet from "./components/Greet";
+import { View, Text, StyleSheet } from "react-native";
 export default function App() {
-  const [showModal, setShowModal] = useState(false);
-  const [showStatusBar, setShowStatusBar] = useState(false);
   return (
-    <View style={{ flex: 1, backgroundColor: "plum", padding: 20 }}>
-      <Greet name="Bruce Wayne" />
+    <View style={styles.container}>
+      <View>
+        <Text>StyleSheet API</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "plum",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
