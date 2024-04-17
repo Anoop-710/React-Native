ef57da9c217c538d6ce322c6ced266276d08b2b3
anoop <anoopaathreya@gmail.com>
2024-04-17 10:30:39 +0530

    Modals

--- App.js
+++ App.js
@@ -5,18 +5,36 @@ import {
   ImageBackground,
   ScrollView,
   Button,
+  Pressable,
+  Modal,
 } from "react-native";
 import logoImage from "./assets/icon.png";
-
+import { useState } from "react";
 export default function App() {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 20 }}>
       <Button
-        title="Press"
-        onPress={() => console.log("Pressed")}
+        title="Open Modal"
+        onPress={() => setShowModal(true)}
         color="midnightblue"
-        disabled
       />
+      <Modal
+        visible={showModal}
+        onRequestClose={() => setShowModal(false)}
+        animationType="slide"
+        // presentationStyle="pageSheet"   only for ios
+      >
+        <View style={{ flex: 1, backgroundColor: "lightblue", padding: 20 }}>
+          <Text>Modal content</Text>
+          <Button
+            title="Close Modal"
+            color="midnightblue"
+            onPress={() => setShowModal(false)}
+          />
+        </View>
+      </Modal>
     </View>
   );
 }
