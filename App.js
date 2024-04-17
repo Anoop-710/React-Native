9a45463f3f9ee67b2783959d031d529f8af0c3b3
anoop <anoopaathreya@gmail.com>
2024-04-17 13:15:07 +0530

    multiple styles

--- App.js
+++ App.js
@@ -2,12 +2,39 @@ import { View, Text, StyleSheet } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <View>
-        <Text>StyleSheet API</Text>
+      <View style={[styles.box, styles.lightblueBg]}>
+        {/* The last element in the array gets the highest precedence */}
+        <Text>Lightblue box</Text>
+      </View>
+      <View style={[styles.box, styles.lightgreenBg]}>
+        <Text>Lightgreen box</Text>
       </View>
     </View>
   );
 }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "plum",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   lightblueBox: {
+//     width: 100,
+//     height: 100,
+//     padding: 10,
+//     backgroundColor: "lightblue",
+//   },
+//   lightgreenBox: {
+//     width: 100,
+//     height: 100,
+//     padding: 10,
+//     backgroundColor: "lightgreen",
+//   },
+// });
+
+//Refactor the code
 
 const styles = StyleSheet.create({
   container: {
@@ -15,5 +42,16 @@ const styles = StyleSheet.create({
     backgroundColor: "plum",
     alignItems: "center",
     justifyContent: "center",
+  },
+  box: {
+    width: 100,
+    height: 100,
+    padding: 10,
+  },
+  lightblueBg: {
+    backgroundColor: "lightblue",
+  },
+  lightgreenBg: {
+    backgroundColor: "lightgreen",
   },
 });
