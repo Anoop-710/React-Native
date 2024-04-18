d12b93bb908e92b1ffd29d8302806181805aa1b8
anoop <anoopaathreya@gmail.com>
2024-04-18 13:41:57 +0530

    flex wrap

--- App.js
+++ App.js
@@ -3,21 +3,11 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "red", alignSelf: "flex-start" }}>
-        Box 1
-      </Box>
-
-      <Box style={{ backgroundColor: "green", alignSelf: "flex-end" }}>
-        Box 2
-      </Box>
-      <Box style={{ backgroundColor: "blue", alignSelf: "center" }}>Box 3</Box>
-      <Box style={{ backgroundColor: "orange", alignSelf: "stretch" }}>
-        Box 4
-      </Box>
-      <Box style={{ backgroundColor: "purple", alignSelf: "baseline" }}>
-        Box 5
-      </Box>
-      {/* align self defaults to auto and the "auto" property is dependent on the alignItems as given below  */}
+      <Box style={{ backgroundColor: "red" }}>Box 1</Box>
+      <Box style={{ backgroundColor: "green" }}>Box 2</Box>
+      <Box style={{ backgroundColor: "blue" }}>Box 3</Box>
+      <Box style={{ backgroundColor: "orange" }}>Box 4</Box>
+      <Box style={{ backgroundColor: "purple" }}>Box 5</Box>
       <Box style={{ backgroundColor: "pink" }}>Box 6</Box>
       <Box style={{ backgroundColor: "gray" }}>Box 7</Box>
     </View>
@@ -26,10 +16,15 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, // takes up all the space on the device
-    alignItems: "flex-end",
-    borderWidth: 8,
+    // flexWrap: "wrap", flexWrap: "nowrap", flexWrap: "wrap-reverse",
+    flexWrap: "wrap",
+    height: 300,
+    borderWidth: 6,
     borderColor: "lightgreen",
     borderRadius: 8,
+    marginTop: 64,
+    // flex: 1,
+    // flexDirection: "row",
+    // flexWrap: "wrap-reverse",
   },
 });
