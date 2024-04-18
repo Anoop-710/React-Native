40b112b6b3639052696a11aa3b8b445bd1baa8c0
anoop <anoopaathreya@gmail.com>
2024-04-18 12:01:49 +0530

    flex

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Box style={{ backgroundColor: "red" }}>Box 1</Box>
+      {/* flex: 1(for box1), flex: 3(for box2)  box 1 takes 25% of the remaining space and box 2 takes 75% */}
+      {/* If box 1 and box 2 both have flex as 1 , they will share the remaining space equally */}
       <Box style={{ backgroundColor: "green" }}>Box 2</Box>
       <Box style={{ backgroundColor: "blue" }}>Box 3</Box>
       <Box style={{ backgroundColor: "yellow" }}>Box 4</Box>
@@ -16,9 +18,10 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1, // takes up all the space on the device
     marginTop: 24,
     borderWidth: 8,
-    borderColor: "black",
+    borderColor: "lightgreen",
     borderRadius: 8,
   },
 });
