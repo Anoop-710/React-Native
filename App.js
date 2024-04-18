0505c0010f74d1b680cafe74920b3a50507d594f
anoop <anoopaathreya@gmail.com>
2024-04-18 11:54:36 +0530

    code setup for layout(flexbox)

--- App.js
+++ App.js
@@ -1,78 +1,24 @@
 import { View, Text, StyleSheet } from "react-native";
+import Box from "./components/Box";
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.darkMode}>
-        <Text style={styles.darkModeText}>
-          Style Inheritance
-          <Text style={styles.boldText}>In Bold</Text>
-        </Text>
-      </View>
-      <View style={[styles.box, styles.lightblueBg, styles.boxShadow]}>
-        {/* The last element in the array gets the highest precedence */}
-        <Text>Lightblue box</Text>
-      </View>
-      <View style={[styles.box, styles.lightgreenBg, styles.androidShadow]}>
-        <Text>Lightgreen box</Text>
-      </View>
+      <Box style={{ backgroundColor: "red" }}>Box 1</Box>
+      <Box style={{ backgroundColor: "green" }}>Box 2</Box>
+      <Box style={{ backgroundColor: "blue" }}>Box 3</Box>
+      <Box style={{ backgroundColor: "yellow" }}>Box 4</Box>
+      <Box style={{ backgroundColor: "purple" }}>Box 5</Box>
+      <Box style={{ backgroundColor: "pink" }}>Box 6</Box>
+      <Box style={{ backgroundColor: "orange" }}>Box 7</Box>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "plum",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  box: {
-    width: 250,
-    height: 250,
-    // padding: 10,
-    paddingHorizontal: 10,
-    paddingVertical: 40,
-    marginVertical: 10,
-    marginHorizontal: 40,
-    borderWidth: 2,
-    borderColor: "purple",
-    borderRadius: 24,
-    // Border radius cannot be applied for TExt in IOS but in android it's possible
-  },
-
-  lightblueBg: {
-    backgroundColor: "lightblue",
-  },
-  lightgreenBg: {
-    backgroundColor: "lightgreen",
-  },
-
-  // applicable for ios
-  boxShadow: {
-    shadowColor: "#333333",
-    shadowOffset: {
-      width: 6,
-      height: 6,
-    },
-    shadowOpacity: 0.6,
-    shadowRadius: 4,
-  },
-
-  // applicable for android
-  androidShadow: {
-    elevation: 10,
-    shadowColor: "red",
-  },
-
-  // Style Inheritance
-  darkMode: {
-    backgroundColor: "black",
-    // color: "white",  Doesn't work as inheritance from view to text is not supported
-  },
-  darkModeText: {
-    color: "white",
-  },
-  boldText: {
-    fontWeight: "bold", // However inheritance within text element is supported
+    marginTop: 24,
+    borderWidth: 8,
+    borderColor: "black",
+    borderRadius: 8,
   },
 });
